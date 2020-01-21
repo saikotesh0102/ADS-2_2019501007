8813ee1fac9ee87c9233c4b0c122256cf392dc15
saikotesh0102 <saikotesh101104@msitprogram.net>
2020-01-21 10:26:54 +0530

    Seam Carver

---
+++ Day - 6/SeamCarver.java
@@ -0,0 +1,54 @@
+public class SeamCarver {
+
+	// create a seam carver object based on the given picture
+	public SeamCarver(Picture picture) {
+
+	}
+ 
+	// current picture
+	public Picture picture() {
+
+	}
+ 
+	// width of current picture
+	public int width() {
+		return -1;
+	}
+ 
+	// height of current picture
+	public int height() {
+		return -1;
+	}
+ 
+	// energy of pixel at column x and row y
+	public double energy(int x, int y) {
+		return null;
+	}
+ 
+	// sequence of indices for horizontal seam
+	public int[] findHorizontalSeam() {
+		return null;
+	}
+ 
+	// sequence of indices for vertical seam
+	public int[] findVerticalSeam() {
+		return null;
+	}
+ 
+	// remove horizontal seam from current picture
+	public void removeHorizontalSeam(int[] seam) {
+
+
+	}
+ 
+	// remove vertical seam from current picture
+	public void removeVerticalSeam(int[] seam) {
+
+	}
+ 
+	//  unit testing (optional)
+	public static void main(String[] args) {
+
+	}
+ 
+ }
